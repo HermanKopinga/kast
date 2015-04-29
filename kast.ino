53e5f3341a015a5a3a7b6588e446f2a334e307f5
Herman Kopinga <herman@kopinga.nl>
2015-04-30 00:02:12 +0200

    Upgraded to use 10bit PWM using TimerOne library.
    Documentation at: http://www.pjrc.com/teensy/td_libs_TimerOne.html

--- kast.ino
+++ kast.ino
@@ -1,9 +1,13 @@
 // LED controller for the light in a bookcase.
 // Herman Kopinga herman@kopinga.nl for 212 Fahrenheit 212f.nl
 
+#include <TimerOne.h>
+
 // Pin definitions.
-#define LEDOUT 12
+#define LEDOUT 14
 #define STATUSLED 11
+#define LIGHTSENSOR A1
+#define MULTIPLIER A2
 
 // Globals
 unsigned long nextBlink;
@@ -13,11 +17,14 @@ void setup() {
   Serial.begin(115200);
   
   pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  pinMode(LIGHTSENSOR, INPUT);
+  pinMode(MULTIPLIER, INPUT);
   pinMode(A3, INPUT);
   pinMode(STATUSLED, OUTPUT);
   pinMode(LEDOUT, OUTPUT);
+  
+  Timer1.initialize(128);
+  Timer1.pwm(LEDOUT, 0);
 }
 
 void loop() {
@@ -29,6 +36,8 @@ void loop() {
   // If sound detected, restart countdown and fade in.
   
   // Measure light intensity, map to output power (through adjustment pot).
-  analogWrite(LEDOUT, 15);
+  int light = analogRead(LIGHTSENSOR);
+  Serial.println(light);
+  Timer1.setPwmDuty(LEDOUT, map(light, 0, 1024, 255, 0)*analogRead(MULTIPLIER)/1024);
 }
 
