ef8e1d115f770211b32be87722be03930f3ffcb2
Herman Kopinga <herman@kopinga.nl>
2015-04-29 23:21:22 +0200

    That was easy. First light :-)

--- kast.ino
+++ kast.ino
@@ -1,11 +1,34 @@
 // LED controller for the light in a bookcase.
 // Herman Kopinga herman@kopinga.nl for 212 Fahrenheit 212f.nl
 
-void setup() {
+// Pin definitions.
+#define LEDOUT 12
+#define STATUSLED 11
 
+// Globals
+unsigned long nextBlink;
+
+void setup() {
+  // Speed is ignored by Teensy.
+  Serial.begin(115200);
+  
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(STATUSLED, OUTPUT);
+  pinMode(LEDOUT, OUTPUT);
 }
 
 void loop() {
-
+  // Blink internal LED: working.
+  if (millis() > nextBlink) {
+    digitalWrite(STATUSLED, !digitalRead(STATUSLED));
+    nextBlink = millis() + 1000;
+  }
+  // If sound detected, restart countdown and fade in.
+  
+  // Measure light intensity, map to output power (through adjustment pot).
+  analogWrite(LEDOUT, 15);
 }
 
